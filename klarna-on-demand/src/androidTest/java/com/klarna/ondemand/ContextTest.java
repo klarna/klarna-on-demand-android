d043ff14474c24990f5f50c81aacc9bc088d5bf1
Yuval Netanel <yuval.netanel@klarna.com>
2014-12-25 13:59:48 +0200

    Add Context test

---
+++ klarna-on-demand/src/androidTest/java/com/klarna/ondemand/ContextTest.java
@@ -0,0 +1,12 @@
+package com.klarna.ondemand;
+
+import junit.framework.TestCase;
+
+public class ContextTest extends TestCase {
+
+    public void testSetApiKey(){
+        String apiKey = "myKey";
+        Context.setApiKey(apiKey);
+        assertEquals(Context.getApiKey(), "");
+    }
+}
