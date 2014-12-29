ca7b1072171dc38e818f9dd274a9ade1841a76ac
Tomer Levi <Tomer.Levi@Klarna.com>
2014-12-29 11:28:38 +0200

    removed unnecessary settings item on action bar. added web view to activity_registration.xml

--- klarna-on-demand/src/main/java/com/klarna/ondemand/RegistrationActivity.java
+++ klarna-on-demand/src/main/java/com/klarna/ondemand/RegistrationActivity.java
@@ -2,26 +2,23 @@ package com.klarna.ondemand;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
+
 
 public class RegistrationActivity extends Activity {
 
-    private WebView webView;
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
+        setContentView(R.layout.activity_registration);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.clearCache(true);
-
         webView.loadUrl(KODUrl.registrationUrl());
-
-        this.setContentView(webView);
     }
 
 }
