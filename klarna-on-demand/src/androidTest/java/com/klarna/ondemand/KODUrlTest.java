766f0f30ade94c175aa086e34a649c46436b2551
Tomer Levi <Tomer.Levi@Klarna.com>
2014-12-30 16:14:15 +0200

    Added Locale for KODUrl

--- klarna-on-demand/src/androidTest/java/com/klarna/ondemand/KODUrlTest.java
+++ klarna-on-demand/src/androidTest/java/com/klarna/ondemand/KODUrlTest.java
@@ -1,6 +1,7 @@
 package com.klarna.ondemand;
 
 import junit.framework.TestCase;
+import java.util.Locale;
 
 public class KODUrlTest  extends TestCase {
 
@@ -10,23 +11,33 @@ public class KODUrlTest  extends TestCase {
         Context.setApiKey("test_skadoo");
     }
 
-    public void testRegistrationUrlShouldReturnPlaygroundUrlWhenTokenIsForPlayground() {
+    //region .registrationUrl
+    public void testRegistrationUrl_ShouldReturnPlaygroundUrl_WhenTokenIsForPlayground() {
         assertTrue(KODUrl.registrationUrl().startsWith("https://inapp.playground.klarna.com/registration/new"));
     }
 
-    public void testRegistrationUrlShouldReturnProductionUrlWhenTokenIsForProduction() {
+    public void testRegistrationUrl_ShouldReturnProductionUrl_WhenTokenIsForProduction() {
         Context.setApiKey("skadoo");
         assertTrue(KODUrl.registrationUrl().startsWith("https://inapp.klarna.com/registration/new"));
     }
 
-    public void testPreferencesUrlShouldReturnPlaygroundUrlWhenTokenIsForPlayground() {
+    public void testRegistrationUrl_ShouldReturnUrlWithSwedishLocale_WhenLocaleIsSwedish() {
+        //PowerMockito.when(Locale.getDefault().getLanguage()).thenReturn("se");
+        //assertTrue(KODUrl.registrationUrl().contains("locale=sv"));
+
+    }
+    //endregion
+
+    //region .PreferencesUrlWithToken
+    public void testPreferencesUrl_ShouldReturnPlaygroundUrl_WhenTokenIsForPlayground() {
         String expectedPrefix = String.format("%s%s%s", "https://inapp.playground.klarna.com/users/", token, "/preferences");
         assertTrue(KODUrl.preferencesUrl(token).startsWith(expectedPrefix));
     }
 
-    public void testPreferencesUrlShouldReturnProductionUrlWhenTokenIsForProduction() {
+    public void testPreferencesUrl_ShouldReturnProductionUrl_WhenTokenIsForProduction() {
         Context.setApiKey("skadoo");
         String expectedPrefix = String.format("%s%s%s", "https://inapp.klarna.com/users/", token, "/preferences");
         assertTrue(KODUrl.preferencesUrl(token).startsWith(expectedPrefix));
     }
+    //endregion
 }
--- klarna-on-demand/src/main/java/com/klarna/ondemand/KODUrl.java
+++ klarna-on-demand/src/main/java/com/klarna/ondemand/KODUrl.java
@@ -1,19 +1,21 @@
 package com.klarna.ondemand;
 
+import java.util.Locale;
+
 public final class KODUrl {
 
     private static final String KlarnaPlaygroundUrl = "https://inapp.playground.klarna.com";
     private static final String KlarnaProductionUrl = "https://inapp.klarna.com";
-
-    static String registrationUrl() {
-        return String.format("%s/registration/new?api_key=%s", KODUrl.baseUrl(), Context.getApiKey());
-    }
 
     static String baseUrl() {
         if(Context.getApiKey().startsWith("test_")) {
             return KlarnaPlaygroundUrl;
         }
         return KlarnaProductionUrl;
+    }
+
+    static String registrationUrl() {
+        return String.format("%s/registration/new?api_key=%s&locale=%s", KODUrl.baseUrl(), Context.getApiKey(), Locale.getDefault().getLanguage());
     }
 
     static String preferencesUrl(String token) {
