929601b901c0e6f34fb7f6e3bb288de1d9c8dbf9
Tomer Levi <Tomer.Levi@Klarna.com>
2015-01-05 11:13:38 +0200

    reverted to JUnit 3

--- klarna-on-demand/src/androidTest/java/com/klarna/ondemand/ContextTest.java
+++ klarna-on-demand/src/androidTest/java/com/klarna/ondemand/ContextTest.java
@@ -1,42 +1,28 @@
 package com.klarna.ondemand;
 
 import junit.framework.TestCase;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
 import java.lang.RuntimeException;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(emulateSdk = 18)
-public class ContextTest{
+public class ContextTest extends TestCase {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Before
-    public void before(){
+    protected void setUp() throws java.lang.Exception {
         Context.setApiKey(null);
     }
 
-    @Test
-    public void setApiKey(){
+    public void testSetApiKey(){
         String apiKey = "my_key";
         Context.setApiKey(apiKey);
-        Assert.assertEquals(Context.getApiKey(), apiKey);
+
+        assertEquals(Context.getApiKey(), apiKey);
     }
 
-    @Test
-    public void getApiKeyShouldThrowExceptionWhenThereIsNoApiKey() {
-        thrown.expect(RuntimeException.class);
-        thrown.expectMessage("You must set the API key first.");
-
-        Context.getApiKey();
+    public void testGetApiKeyShouldThrowExceptionWhenThereIsNoApiKey() {
+        try {
+            Context.getApiKey();
+            fail("Exception missing");
+        }
+        catch(RuntimeException e) {
+            assertEquals(e.getMessage(), "You must set the API key first.");
+        }
     }
 }
--- klarna-on-demand/src/androidTest/java/com/klarna/ondemand/UrlHelperTest.java
+++ klarna-on-demand/src/androidTest/java/com/klarna/ondemand/UrlHelperTest.java
@@ -1,64 +1,49 @@
 package com.klarna.ondemand;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
+import junit.framework.TestCase;
 import java.util.Locale;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(emulateSdk = 18)
-public class UrlHelperTest{
+public class UrlHelperTest extends TestCase {
 
     String token = "my_token";
 
-    @Before
     public void setUp() throws java.lang.Exception {
         Context.setApiKey("test_skadoo");
     }
 
     //region .registrationUrl
-    @Test
     public void testRegistrationUrl_ShouldReturnPlaygroundUrl_WhenTokenIsForPlayground() {
-        Assert.assertTrue(UrlHelper.registrationUrl().startsWith("https://inapp.playground.klarna.com/registration/new"));
+        assertTrue(UrlHelper.registrationUrl().startsWith("https://inapp.playground.klarna.com/registration/new"));
     }
 
-    @Test
     public void testRegistrationUrl_ShouldReturnProductionUrl_WhenTokenIsForProduction() {
         Context.setApiKey("skadoo");
-        Assert.assertTrue(UrlHelper.registrationUrl().startsWith("https://inapp.klarna.com/registration/new"));
+        assertTrue(UrlHelper.registrationUrl().startsWith("https://inapp.klarna.com/registration/new"));
     }
 
-    @Test
     public void testRegistrationUrl_ShouldReturnUrlWithSwedishLocale_WhenLocaleIsSwedish() {
         Locale.setDefault(new Locale("sv"));
 
-        Assert.assertTrue(UrlHelper.registrationUrl().contains("locale=sv"));
+        assertTrue(UrlHelper.registrationUrl().contains("locale=sv"));
     }
     //endregion
 
     //region .PreferencesUrlWithToken
-    @Test
     public void testPreferencesUrlWithToken_ShouldReturnPlaygroundUrl_WhenTokenIsForPlayground() {
         String expectedPrefix = String.format("%s%s%s", "https://inapp.playground.klarna.com/users/", token, "/preferences");
-        Assert.assertTrue(UrlHelper.preferencesUrl(token).startsWith(expectedPrefix));
+        assertTrue(UrlHelper.preferencesUrl(token).startsWith(expectedPrefix));
     }
 
-    @Test
     public void testPreferencesUrlWithToken_ShouldReturnProductionUrl_WhenTokenIsForProduction() {
         Context.setApiKey("skadoo");
         String expectedPrefix = String.format("%s%s%s", "https://inapp.klarna.com/users/", token, "/preferences");
-        Assert.assertTrue(UrlHelper.preferencesUrl(token).startsWith(expectedPrefix));
+        assertTrue(UrlHelper.preferencesUrl(token).startsWith(expectedPrefix));
     }
 
-    @Test
     public void testPreferencesUrlWithToken_ShouldReturnUrlWithSwedishLocale_WhenLocaleIsSwedish() {
         Locale.setDefault(new Locale("sv"));
 
-        Assert.assertTrue(UrlHelper.preferencesUrl(token).contains("locale=sv"));
+        assertTrue(UrlHelper.preferencesUrl(token).contains("locale=sv"));
     }
     //endregion
 }
