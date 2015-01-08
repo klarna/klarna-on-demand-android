63636314c2b94eaf8b4b22c2bfe43ca349d7fbc4
Tomer Levi <Tomer.Levi@Klarna.com>
2015-01-08 17:17:00 +0200

    Switched from Easymock to Mockito.

--- klarna-on-demand/src/test/java/com/klarna/ondemand/ContextTest.java
+++ klarna-on-demand/src/test/java/com/klarna/ondemand/ContextTest.java
@@ -2,19 +2,22 @@ package com.klarna.ondemand;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.robolectric.annotation.Config;
-
-import java.lang.RuntimeException;
 
 @RunWith(PowerMockRunner.class)
 @Config(emulateSdk = 18)
 public class ContextTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Before
-    public void setUp() throws java.lang.Exception {
+    public void beforeEach() throws java.lang.Exception {
         Context.setApiKey(null);
     }
 
@@ -27,12 +30,9 @@ public class ContextTest {
 
     @Test
     public void getApiKeyShouldThrowExceptionWhenThereIsNoApiKey() {
-        try {
-            Context.getApiKey();
-            Assert.fail("Exception missing");
-        }
-        catch(RuntimeException e) {
-            Assert.assertEquals(e.getMessage(), "You must set the API key first.");
-        }
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("You must set the API key first.");
+
+        Context.getApiKey();
     }
 }
--- klarna-on-demand/src/test/java/com/klarna/ondemand/UrlHelperTest.java
+++ klarna-on-demand/src/test/java/com/klarna/ondemand/UrlHelperTest.java
@@ -1,23 +1,31 @@
 package com.klarna.ondemand;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.robolectric.annotation.Config;
 
 import java.util.Locale;
 
+import static org.powermock.api.mockito.PowerMockito.when;
+
 @RunWith(PowerMockRunner.class)
 @Config(emulateSdk = 18)
+@PrepareForTest(Context.class)
 public class UrlHelperTest {
 
     String token = "my_token";
 
     @Before
-    public void setUp() throws java.lang.Exception {
-        Context.setApiKey("test_skadoo");
+    public void beforeEach() {
+        PowerMockito.mockStatic(Context.class);
+        when(Context.getApiKey()).thenReturn("test_skadoo");
     }
 
     //region .registrationUrl
@@ -28,7 +36,9 @@ public class UrlHelperTest {
 
     @Test
     public void registrationUrl_ShouldReturnProductionUrl_WhenTokenIsForProduction() {
-        Context.setApiKey("skadoo");
+        PowerMockito.mockStatic(Context.class);
+        when(Context.getApiKey()).thenReturn("skadoo");
+
         Assert.assertTrue(UrlHelper.registrationUrl().startsWith("https://inapp.klarna.com/registration/new"));
     }
 
@@ -49,7 +59,9 @@ public class UrlHelperTest {
 
     @Test
     public void preferencesUrlWithToken_ShouldReturnProductionUrl_WhenTokenIsForProduction() {
-        Context.setApiKey("skadoo");
+        PowerMockito.mockStatic(Context.class);
+        when(Context.getApiKey()).thenReturn("skadoo");
+
         String expectedPrefix = String.format("%s%s%s", "https://inapp.klarna.com/users/", token, "/preferences");
         Assert.assertTrue(UrlHelper.preferencesUrl(token).startsWith(expectedPrefix));
     }
