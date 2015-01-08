9be79aa6974c2cc033a09985eb4d2ab4e0194f78
Tomer Levi <Tomer.Levi@Klarna.com>
2015-01-08 17:17:00 +0200

    Switched from Easymock to Mockito.

--- klarna-on-demand/src/test/java/com/klarna/ondemand/ContextTest.java
+++ klarna-on-demand/src/test/java/com/klarna/ondemand/ContextTest.java
@@ -2,37 +2,33 @@ package com.klarna.ondemand;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.robolectric.annotation.Config;
-
-import java.lang.RuntimeException;
 
 @RunWith(PowerMockRunner.class)
 @Config(emulateSdk = 18)
 public class ContextTest {
 
-    @Before
-    public void setUp() throws java.lang.Exception {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void setApiKey() {
+        String apiKey = "my_key";
+        Context.setApiKey(apiKey);
+        Assert.assertEquals(Context.getApiKey(), apiKey);
         Context.setApiKey(null);
     }
 
     @Test
-    public void setApiKey(){
-        String apiKey = "my_key";
-        Context.setApiKey(apiKey);
-        Assert.assertEquals(Context.getApiKey(), apiKey);
-    }
-
-    @Test
     public void getApiKeyShouldThrowExceptionWhenThereIsNoApiKey() {
-        try {
-            Context.getApiKey();
-            Assert.fail("Exception missing");
-        }
-        catch(RuntimeException e) {
-            Assert.assertEquals(e.getMessage(), "You must set the API key first.");
-        }
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("You must set the API key first.");
+
+        Context.getApiKey();
     }
 }
