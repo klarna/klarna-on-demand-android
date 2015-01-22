a022494172be3f619aafb98bfa1eb0473360a053
Yuval Netanel <yuval.netanel@klarna.com>
2015-01-22 19:05:38 +0200

    Fix typo

--- klarna-on-demand/src/test/java/com/klarna/ondemand/OriginProofTest.java
+++ klarna-on-demand/src/test/java/com/klarna/ondemand/OriginProofTest.java
@@ -77,7 +77,7 @@ public class OriginProofTest {
         assertThat(dataA.getString("id")).isNotEqualTo(dataB.getString("id"));
     }
 
-    @Test(ex    pected=RuntimeException.class)
+    @Test(expected=RuntimeException.class)
     public void constructor_ShouldThrowExceptionWhenItCantGenerateSignature() throws Exception {
         Crypto cryptoMock = mock(Crypto.class);
         when(cryptoMock.sign(anyString())).thenThrow(new SignatureException());
