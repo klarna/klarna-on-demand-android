cb0868bcfa944cb55c3ad546ab21b8d902baf2ea
Tomer Levi <Tomer.Levi@Klarna.com>
2015-01-27 16:02:14 +0200

    send post request to backend server and show qr-code / error message.

--- example/src/main/java/com/klarna/example/MainActivity.java
+++ example/src/main/java/com/klarna/example/MainActivity.java
@@ -6,32 +6,46 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.klarna.ondemand.OriginProof;
 import com.klarna.ondemand.PreferencesActivity;
 import com.klarna.ondemand.RegistrationActivity;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class MainActivity extends Activity {
 
     public static final int REGISTRATION_REQUEST_CODE = 1;
     public static final int PREFERENCES_REQUEST_CODE = 2;
     private static final String USER_TOKEN_KEY = "userToken";
-    private View registerButton;
-    private View preferencesButton;
-    private View buyButton;
-    private AlertDialog alert;
+    
+    private View changePaymentTextView;
+    private View registerTextView;
+    private View buyTextView;
+    private View qrCodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
+        
         com.klarna.ondemand.Context.setApiKey("test_d8324b98-97ce-4974-88de-eaab2fdf4f14");
 
 
-        initElements();
+        initializeUIElements();
         updateUIElements();
     }
 
@@ -65,26 +79,45 @@ public class MainActivity extends Activity {
             }
         }
     }
+    
+    //region UI behaviours
 
-    private void initElements() {
-        registerButton = findViewById(R.id.registerButton);
-        preferencesButton = findViewById(R.id.preferencesButton);
-        buyButton = findViewById(R.id.buyButton);
-
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("origin proof");
-        builder.setMessage(null);
-
-        alert = builder.create();
+    private void initializeUIElements() {
+        registerTextView = findViewById(R.id.registerTextView);
+        buyTextView = findViewById(R.id.buyTextView);
+        changePaymentTextView = findViewById(R.id.changePaymentTextView);
+        qrCodeView = findViewById(R.id.qrCodeView);
     }
 
     private void updateUIElements() {
-        registerButton.setVisibility(hasUserToken() == false ? View.VISIBLE : View.INVISIBLE);
-        preferencesButton.setVisibility(hasUserToken() == true ? View.VISIBLE : View.INVISIBLE);
-        buyButton.setVisibility(hasUserToken() == true ? View.VISIBLE : View.INVISIBLE);
+        registerTextView.setVisibility(hasUserToken() == false ? View.VISIBLE : View.INVISIBLE);
+        changePaymentTextView.setVisibility(hasUserToken() == true ? View.VISIBLE : View.INVISIBLE);
     }
 
-    public void openKlarnaRegistration(View view) {
+    public void showQRCode() {
+        qrCodeView.setVisibility(View.VISIBLE);
+    }
+
+    public void hideQRCode(View view) {
+        qrCodeView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showAlert(String message) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(this.getTitle());
+        builder.setMessage(message);
+        builder.setPositiveButton("OK",null);
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    
+    //endregion
+    
+    //region Purchase using Klarna
+
+    public void openKlarnaRegistration() {
         Intent intent = new Intent(this, RegistrationActivity.class);
         startActivityForResult(intent, REGISTRATION_REQUEST_CODE);
     }
@@ -93,14 +126,81 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this, PreferencesActivity.class);
         intent.putExtra(PreferencesActivity.EXTRA_USER_TOKEN, getUserToken());
         startActivityForResult(intent, PREFERENCES_REQUEST_CODE);
-
     }
 
-    public void onBuy(View view) {
-        OriginProof originProof = new OriginProof(3600, "SEK", getUserToken(), getApplicationContext());
-        alert.setMessage(originProof.toString());
-        alert.show();
+    public void onBuyPressed(View view) {
+        if(this.hasUserToken()) {
+            OriginProof originProof = new OriginProof(3600, "SEK", getUserToken(), getApplicationContext());
+
+            class purchaseItemRunnable implements Runnable {
+                String reference;
+                OriginProof originProof;
+
+                purchaseItemRunnable(String reference, OriginProof originProof) {
+                    this.reference = reference;
+                    this.originProof = originProof;
+                }
+
+                @Override
+                public void run() {
+                    try {
+                        performPurchaseOfItem(reference, originProof);
+                    } catch (final Exception e) {
+                        Handler mainHandler = new Handler(getApplicationContext().getMainLooper());
+
+                        mainHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                showAlert("Error: " + e.toString());
+                            }
+                        });
+                    }
+                }
+            }
+            
+            Thread thread = new Thread(new purchaseItemRunnable("TCKT0001", originProof));
+            thread.start();
+        }
+        else {
+            openKlarnaRegistration();
+        }
     }
+
+    private void performPurchaseOfItem(String reference, OriginProof originProof) throws IOException, JSONException {
+        HttpClient httpClient = new DefaultHttpClient();
+
+        HttpPost httpPost = new HttpPost("http://192.168.56.1:9292/pay");
+
+        JSONObject jsonParams = new JSONObject();
+        jsonParams.put("originProof", originProof.toString());
+        jsonParams.put("reference", reference);
+        jsonParams.put("user_token", getUserToken());
+        
+        StringEntity params = new StringEntity(jsonParams.toString());
+        params.setContentType("application/json;charset=UTF-8");
+        params.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
+
+        httpPost.setEntity(params);
+
+        final HttpResponse response = httpClient.execute(httpPost);
+        
+        Handler mainHandler = new Handler(getApplicationContext().getMainLooper());
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode >= 200 && statusCode < 300) {
+                     showQRCode();
+                }
+                else {
+                     showAlert("Error: " + response.toString());
+                }
+            }});
+    }
+    
+    //endregion
+    
+    //region User token persistence
 
     private void saveUserToken(String token) {
         SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
@@ -118,4 +218,6 @@ public class MainActivity extends Activity {
     private boolean hasUserToken() {
         return getUserToken() != null;
     }
+    
+    //endregion
 }
