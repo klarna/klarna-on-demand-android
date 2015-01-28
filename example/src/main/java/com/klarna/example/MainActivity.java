a1bf8f39e70ee29004893e05d0ea5ba7d1df9a74
Tomer Levi <Tomer.Levi@Klarna.com>
2015-01-28 18:12:55 +0200

    added documentation for main activity

--- example/src/main/java/com/klarna/example/MainActivity.java
+++ example/src/main/java/com/klarna/example/MainActivity.java
@@ -15,12 +15,10 @@ import com.klarna.ondemand.PreferencesActivity;
 import com.klarna.ondemand.RegistrationActivity;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,7 +32,6 @@ public class MainActivity extends Activity {
     
     private View registerTextView;
     private View changePaymentButton;
-    private View buyButton;
     private View qrCodeView;
 
     @Override
@@ -45,7 +42,6 @@ public class MainActivity extends Activity {
         
         com.klarna.ondemand.Context.setApiKey("test_d8324b98-97ce-4974-88de-eaab2fdf4f14");
 
-
         initializeUIElements();
         updateUIElements();
     }
@@ -55,9 +51,14 @@ public class MainActivity extends Activity {
         if (requestCode == REGISTRATION_REQUEST_CODE) {
             switch (resultCode) {
                 case RegistrationActivity.RESULT_OK:
+                    // Get user token extra parameter
                     String token = data.getStringExtra(RegistrationActivity.EXTRA_USER_TOKEN);
+                    
+                    // Save user token for future-use, in order to identify the user.
                     saveUserToken(token);
+                    
                     updateUIElements();
+                    
                     buyTicket();
                     break;
                 case RegistrationActivity.RESULT_CANCELED:
@@ -86,7 +87,6 @@ public class MainActivity extends Activity {
 
     private void initializeUIElements() {
         registerTextView = findViewById(R.id.registerTextView);
-        buyButton = findViewById(R.id.buyButton);
         changePaymentButton = findViewById(R.id.changePaymentButton);
         qrCodeView = findViewById(R.id.qrCodeView);
     }
@@ -107,38 +107,38 @@ public class MainActivity extends Activity {
     private void showAlert(String message) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setTitle(this.getTitle());
-        builder.setMessage(message);
-        builder.setPositiveButton("OK",null);
-
-        AlertDialog alert = builder.create();
-        alert.show();
+        builder.setTitle(getTitle())
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.alertDefaultPositionButtonText), null)
+                .setCancelable(true)
+                .create()
+                .show();
     }
     
     //endregion
     
     //region Purchase using Klarna
 
-    public void openKlarnaRegistration() {
-        Intent intent = new Intent(this, RegistrationActivity.class);
-        startActivityForResult(intent, REGISTRATION_REQUEST_CODE);
-    }
-
     public void openKlarnaPreferences(View view) {
+        // Start preferences activity with user token that was saved when the user completed the registration process.
         Intent intent = new Intent(this, PreferencesActivity.class);
         intent.putExtra(PreferencesActivity.EXTRA_USER_TOKEN, getUserToken());
         startActivityForResult(intent, PREFERENCES_REQUEST_CODE);
     }
 
     public void onBuyPressed(View view) {
+        // if a token has not been previously created
         if (this.hasUserToken()) {
             buyTicket();
         } else {
-            openKlarnaRegistration();
+            // open Klarna registration
+            Intent intent = new Intent(this, RegistrationActivity.class);
+            startActivityForResult(intent, REGISTRATION_REQUEST_CODE);
         }
     }
     
     private void buyTicket() {
+        // Create a command for purchasing item in a background thread. 
         class purchaseItemRunnable implements Runnable {
             String reference;
             OriginProof originProof;
@@ -158,24 +158,28 @@ public class MainActivity extends Activity {
                     mainHandler.post(new Runnable() {
                         @Override
                         public void run() {
-                            showAlert("Error: " + e.toString());
+                            // Log and display error.
                             Log.e(getClass().getName(), "Error on performPurchaseOfItem", e);
+                            showAlert("Error: " + e.toString());
                         }
                     });
                 }
             }
         }
 
+        // create origin proof for order.
         OriginProof originProof = new OriginProof(3600, "SEK", getUserToken(), getApplicationContext());
 
+        // run background thread with purchase item command.
         Thread thread = new Thread(new purchaseItemRunnable("TCKT0001", originProof));
         thread.start();
     }
 
-    private void performPurchaseOfItem(String reference, OriginProof originProof) throws IOException, JSONException {
-        HttpClient httpClient = new DefaultHttpClient();
-
-        HttpPost httpPost = new HttpPost("http://192.168.56.1:9292/pay");
+    private void performPurchaseOfItem(String reference, OriginProof originProof) throws IOException, JSONException, HttpHostConnectException {
+        // Create post request with backend server's path for payments.
+        
+        //for Genymotion devices, use the following path: http://10.0.2.2:9292/pay.
+        HttpPost httpPost = new HttpPost("http://10.0.2.2:9292/pay");
 
         JSONObject jsonParams = new JSONObject();
         jsonParams.put("originProof", originProof.toString());
@@ -184,23 +188,25 @@ public class MainActivity extends Activity {
         
         StringEntity params = new StringEntity(jsonParams.toString());
         params.setContentType("application/json;charset=UTF-8");
-        params.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
 
         httpPost.setEntity(params);
 
-        final HttpResponse response = httpClient.execute(httpPost);
+        final HttpResponse response = new DefaultHttpClient().execute(httpPost);
         
+        // Handle response on UI thread (main).
         Handler mainHandler = new Handler(getApplicationContext().getMainLooper());
         mainHandler.post(new Runnable() {
             @Override
             public void run() {
                 int statusCode = response.getStatusLine().getStatusCode();
                 if (statusCode >= 200 && statusCode < 300) {
-                     showQRCode();
+                    // show QR Code for the movie.
+                    showQRCode();
                 }
                 else {
-                     showAlert("Error: " + response.toString());
-                     Log.e(getClass().getName(), response.toString());
+                    // Log and display error.
+                    Log.e(getClass().getName(), response.toString());
+                    showAlert("Error: " + response.toString());
                 }
             }});
     }
