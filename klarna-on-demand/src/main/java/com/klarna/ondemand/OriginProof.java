2e6c559fdf1b7b5b2b0edbe65239f79a25942ace
moria <moria.ahi-mordehai@klarna.com>
2016-02-11 16:54:50 +0200

    WIP

--- klarna-on-demand/src/main/java/com/klarna/ondemand/OriginProof.java
+++ klarna-on-demand/src/main/java/com/klarna/ondemand/OriginProof.java
@@ -2,6 +2,7 @@ package com.klarna.ondemand;
 
 import android.util.Base64;
 
+import com.klarna.ondemand.crypto.CryptoBase;
 import com.klarna.ondemand.crypto.CryptoFactory;
 
 import org.json.JSONObject;
@@ -16,8 +17,29 @@ public class OriginProof {
     private final int amount;
     private final String currency;
     private final String userToken;
+    private final String externalPrivateKey;
     private final UUID uuid;
     private final String originProofBase64Str;
+
+    private OriginProof(int amount, String currency, String userToken, String externalPrivateKey, android.content.Context context) {
+        this.amount = amount;
+        this.currency = currency;
+        this.userToken = userToken;
+        this.context = context;
+        this.externalPrivateKey = externalPrivateKey;
+        this.uuid = UUID.randomUUID();
+        originProofBase64Str = generateBase64Str();
+    }
+    /**
+     * Generates an origin proof that can be used to verify the details of the purchase.
+     * @param amount             The purchase's total amount in "cents"
+     * @param currency           The currency used in this purchase ("SEK", "EUR", or other <a href="http://en.wikipedia.org/wiki/ISO_4217">ISO 4217 codes</a>)
+     * @param userToken          Token identifying the user making the purchase
+     * @param externalPrivateKey Private key stored in device, in case cryptographic keys generation is performed outside of the SDK
+     */
+    public OriginProof(int amount, String currency, String userToken, String externalPrivateKey) {
+        this(amount, currency, userToken, externalPrivateKey, null);
+    }
 
     /**
      * Generates an origin proof that can be used to verify the details of the purchase.
@@ -27,12 +49,7 @@ public class OriginProof {
      * @param context   The android context
      */
     public OriginProof(int amount, String currency, String userToken, android.content.Context context) {
-        this.amount = amount;
-        this.currency = currency;
-        this.userToken = userToken;
-        this.context = context;
-        this.uuid = UUID.randomUUID();
-        originProofBase64Str = generateBase64Str();
+        this(amount, currency, userToken, null, context);
     }
 
     /**
@@ -52,7 +69,14 @@ public class OriginProof {
                     .put("currency", currency)
                     .put("user_token", userToken)
                     .put("id", uuid.toString());
-            String signature = CryptoFactory.getInstance(context).sign(data.toString());
+
+            String signature = null;
+            if(externalPrivateKey == null) {
+                signature = CryptoFactory.getInstance(context).sign(data.toString());
+            }
+            else {
+                signature = CryptoBase.sign(data.toString(), externalPrivateKey);
+            }
 
             JSONObject originProof = new JSONObject()
                     .put("data", data.toString())
--- klarna-on-demand/src/main/java/com/klarna/ondemand/crypto/CryptoBase.java
+++ klarna-on-demand/src/main/java/com/klarna/ondemand/crypto/CryptoBase.java
@@ -2,14 +2,18 @@ package com.klarna.ondemand.crypto;
 
 import android.util.Base64;
 
+import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
 
-abstract class CryptoBase implements Crypto {
+public abstract class CryptoBase implements Crypto {
 
     private static final String DIGEST_ALGORITHM = "SHA256withRSA";
 
@@ -24,15 +28,33 @@ abstract class CryptoBase implements Crypto {
         return publicKeyBase64Str;
     }
 
-    @Override
-    public String sign(String message) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    public static String sign(String message, PrivateKey privateKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
         Signature sign = Signature.getInstance(DIGEST_ALGORITHM);
-        sign.initSign(getPrivateKey());
+        sign.initSign(privateKey);
         sign.update(message.getBytes());
         return new String(Base64.encode(sign.sign(), Base64.DEFAULT));
+    }
+
+    public static String sign(String message, String privateKey) throws GeneralSecurityException {
+        return sign(message, loadPrivateKey(privateKey));
+    }
+
+    @Override
+    public String sign(String message) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        return sign(message, getPrivateKey());
     }
 
     protected PrivateKey getPrivateKey() {
         return privateKey;
     }
+
+    public static PrivateKey loadPrivateKey(String key64) throws GeneralSecurityException {
+        byte[] clear = Base64.decode(key64, Base64.DEFAULT);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(clear);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey generatedKey = keyFactory.generatePrivate(keySpec);
+        Arrays.fill(clear, (byte) 0);
+        return generatedKey;
+    }
+
 }
